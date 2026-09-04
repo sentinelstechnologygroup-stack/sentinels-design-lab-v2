abfefb0907649ebc0e94a6be8971d0fa1434435d
sentinelstechnologygroup-stack <sentinelstechnologygroup@gmail.com>
2026-09-03 19:29:14 -0500

    fix: include dashboard components in Tailwind build

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   darkMode: ["class"],
   content: [
     "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
     "./src/**/*.{js,jsx,ts,tsx,mdx}",
   ],
   theme: {
